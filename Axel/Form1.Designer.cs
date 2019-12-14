5ace28d435b87db445f5c863fc314b575645881c
Andrey Borodin <amborodin@acm.org>
2019-12-14 17:45:02 +0500

    Some code

---
+++ Axel/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Axel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraCharts.XYDiagram xyDiagram1 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.SplineSeriesView splineSeriesView1 = new DevExpress.XtraCharts.SplineSeriesView();
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.keyDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.valueDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.axelDataBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(splineSeriesView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axelDataBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartControl1
+            // 
+            this.chartControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1";
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1";
+            this.chartControl1.Diagram = xyDiagram1;
+            this.chartControl1.Legend.Name = "Default Legend";
+            this.chartControl1.Location = new System.Drawing.Point(399, 12);
+            this.chartControl1.Name = "chartControl1";
+            series1.Name = "Series 1";
+            series1.View = splineSeriesView1;
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series1};
+            this.chartControl1.Size = new System.Drawing.Size(736, 632);
+            this.chartControl1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(68, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(224, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "-y";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(125, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "y";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Производная";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.keyDataGridViewTextBoxColumn,
+            this.valueDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.axelDataBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 189);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(381, 455);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(372, 46);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Нарисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // keyDataGridViewTextBoxColumn
+            // 
+            this.keyDataGridViewTextBoxColumn.DataPropertyName = "Key";
+            this.keyDataGridViewTextBoxColumn.HeaderText = "";
+            this.keyDataGridViewTextBoxColumn.Name = "keyDataGridViewTextBoxColumn";
+            this.keyDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // valueDataGridViewTextBoxColumn
+            // 
+            this.valueDataGridViewTextBoxColumn.DataPropertyName = "Value";
+            this.valueDataGridViewTextBoxColumn.HeaderText = "Значение";
+            this.valueDataGridViewTextBoxColumn.Name = "valueDataGridViewTextBoxColumn";
+            // 
+            // axelDataBindingSource
+            // 
+            this.axelDataBindingSource.DataSource = typeof(Axel.AxelData);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1147, 666);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chartControl1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(splineSeriesView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axelDataBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraCharts.ChartControl chartControl1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource axelDataBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn keyDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn valueDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Axel/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.Utils;
+using DevExpress.XtraCharts;
+using NCalc;
+
+namespace Axel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RebuildDS();
+            Draw();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            RebuildDS();
+        }
+
+        private void RebuildDS()
+        {
+            var old = dataGridView1.DataSource as AxelData ?? new AxelData();
+            var ad = new AxelData();
+            label1.Text = "y";
+            for (int i = 0; i < numericUpDown1.Value; i++)
+            {
+                var key = label1.Text;
+                HaveParameter(ad, key, old, i == 0 ? 1 : 0);
+                label1.Text += "'";
+            }
+
+            label1.Text += "=";
+
+            HaveParameter(ad, "Xmin", old, 0);
+            HaveParameter(ad, "Xmax", old, 10);
+            HaveParameter(ad, "Шагов", old, 1000);
+            HaveParameter(ad, "Ymin", old, -10000);
+            HaveParameter(ad, "Ymax", old, 10000);
+
+            ad.Order = (int) numericUpDown1.Value;
+            ad.Expression = textBox1.Text;
+            dataGridView1.DataSource = ad;
+        }
+
+        private static void HaveParameter(AxelData ad, string key, AxelData old, double def = 0)
+        {
+            ad.Add(new AxelRow()
+            {
+                Key = key,
+                Value = old.Where(k => k.Key == key).Select(k => (double?) k.Value).FirstOrDefault() ?? def
+            });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RebuildDS();
+            Draw();
+        }
+
+        private void Draw()
+        {
+            chartControl1.BeginInit();
+            var series = this.chartControl1.Series.First();
+            series.ActualPoints.Clear();
+            foreach (var tuple in ((AxelData) dataGridView1.DataSource).Compute())
+            {
+                series.ActualPoints.Add(new SeriesPoint(tuple.Item1, tuple.Item2));
+            }
+
+            chartControl1.EndInit();
+        }
+    }
+
+    public class AxelRow
+    {
+        public string Key { get; set; }
+        public double Value { get; set; }
+    }
+
+    public class AxelData : List<AxelRow>
+    {
+        public int Order { get; set; }
+        public string Expression { get; set; }
+
+        public IEnumerable<Tuple<double, double>> Compute()
+        {
+            double x = Get("Xmin");
+            double xmax = Get("Xmax");
+            double ymin = Get("Ymin");
+            double ymax = Get("Ymax");
+            double steps = Get("Шагов");
+            double[] y = new double[Order + 1];
+            string literal = "y";
+            for (int i = 0; i < Order; i++)
+            {
+                y[i] = Get(literal);
+                literal += "'";
+            }
+
+            double step = (xmax - x) / steps;
+            if (step <= 0)
+                yield break;
+
+            var expr = new Expression(Expression);
+            if (expr.HasErrors())
+                yield break;
+            expr.EvaluateParameter += delegate(string name, ParameterArgs args)
+            {
+                switch (name)
+                {
+                    case "x":
+                        args.Result = x;
+                        break;
+                    case "y":
+                        args.Result = y[0];
+                        break;
+                    case "dt":
+                        args.Result = step;
+                        break;
+                }
+
+                if (name.StartsWith("y"))
+                {
+                    var num = name.Substring(1);
+                    int idx;
+                    if (int.TryParse(num, out idx) && idx <= Order)
+                    {
+                        args.Result = y[idx];
+                    }
+                }
+            };
+
+            for (; x <= xmax; x += step)
+            {
+                y[Order] = Convert.ToDouble(expr.Evaluate());
+                for (int i = 0; i < Order; i++)
+                {
+                    y[i] += y[i + 1] * step;
+                }
+
+                if (y[0] >= ymin && y[0] <= ymax)
+                    yield return Tuple.Create(x, y[0]);
+            }
+
+            yield return Tuple.Create(x, y[0]);
+
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка рассчёта\n" + ex.ToString(), "Ошибка");
+            }
+        }
+
+        private double Get(string key)
+        {
+            return this.Where(k => k.Key == key).Select(k => k.Value).FirstOrDefault();
+        }
+    }
+}
